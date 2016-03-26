822659617cf6378318ab5370618cb25d0ffa163f
emilieg <emilie.gerber@hotmail.com>
2016-03-26 16:01:53 -0700

    added function to update cell

--- main.js
+++ main.js
@@ -6,14 +6,19 @@ var flipTurn = function() {
     playerTurn = "X";
   }
 }
-
+var updateCell = function(cellNum, playerTurn) {
+  var cellId = "cell" + cellNum;
+  document.getElementById(cellId).innerHTML = playerTurn;
+}
 
 
 var gameLoop = function(cellNum) {
   console.log("you clicked on "+ cellNum);
 
   console.log("it is playerTurn " + playerTurn);
+   updateCell(cellNum, playerTurn);
    flipTurn();
+
 }
 
 
@@ -22,46 +27,37 @@ var gameLoop = function(cellNum) {
 document.getElementById("square1").addEventListener("click", function() {  
         gameLoop(1);
 
- // document.getElementById("cell1").innerHTML = "X";
 })
 
 document.getElementById("square2").addEventListener("click", function() {  
         gameLoop(2);
 
- // document.getElementById("cell1").innerHTML = "X";
 })
 document.getElementById("square3").addEventListener("click", function() {  
         gameLoop(3);
 
- // document.getElementById("cell1").innerHTML = "X";
 })
 document.getElementById("square4").addEventListener("click", function() {  
         gameLoop(4);
 
- // document.getElementById("cell1").innerHTML = "X";
 })
 document.getElementById("square5").addEventListener("click", function() {  
         gameLoop(5);
 
- // document.getElementById("cell1").innerHTML = "X";
 })
 document.getElementById("square6").addEventListener("click", function() {  
         gameLoop(6);
 
- // document.getElementById("cell1").innerHTML = "X";
 })
 document.getElementById("square7").addEventListener("click", function() {  
         gameLoop(7);
 
- // document.getElementById("cell1").innerHTML = "X";
 })
 document.getElementById("square8").addEventListener("click", function() {  
         gameLoop(8);
 
- // document.getElementById("cell1").innerHTML = "X";
 })
 document.getElementById("square9").addEventListener("click", function() {  
         gameLoop(9);
 
- // document.getElementById("cell1").innerHTML = "X";
 })

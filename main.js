82eb86e78e882c6d01d26dec5c92336f81351040
emilieg <emilie.gerber@hotmail.com>
2016-03-26 11:15:24 -0700

    created a how to file, started on js event listeners

---
+++ main.js
@@ -0,0 +1,15 @@
+//need to set up .innerHTML to a variable that changes/alternates
+//with moves X to O and O to X. 
+
+document.getElementById('square1').addEventListener("click", function() {  
+
+ document.getElementById("cell1").innerHTML ="X";
+})
+document.getElementById('square2').addEventListener("click", function() {  
+
+ document.getElementById("cell2").innerHTML ="X";
+})
+document.getElementById('square3').addEventListener("click", function() {  
+
+ document.getElementById("cell3").innerHTML ="X";
+})
